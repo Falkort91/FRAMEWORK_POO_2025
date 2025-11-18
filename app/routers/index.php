0c563a21cc3fce58d590459280458ada4418e67d
Falkort <lleclercq91@outlook.com>
2025-11-18 10:07:37 +0100

    Route pages.home

--- app/routers/index.php
+++ app/routers/index.php
@@ -4,4 +4,5 @@
 //PATERN: /
 //CONTROLLER: pagesController
 //ACTION: home
-
+include_once '../app/controllers/pagesController.php';
+\App\Controllers\PagesController\homeAction($conn);
---
+++ app/views/pages/home.php
@@ -0,0 +1,11 @@
+<h2>Recent books</h2>
+<ul>
+    <?php foreach($books as $book):?>
+    <li>
+        <?php echo $book['title'];?>
+    </li>
+    <li>
+        <?php echo $book['resume'];?>
+    </li>
+    <?php endforeach?>
+</ul>
